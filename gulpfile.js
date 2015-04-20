6cdd6e6daa4ddc81a6b11751398ead2cc2cd1e3d
n4bcak3 <nabcak3@gmail.com>
2015-04-20 09:19:20 -0700

    cards and newletters

--- gulpfile.js
+++ gulpfile.js
@@ -17,11 +17,11 @@ gulp.task('app', function(){
     ]))
     .pipe(concat('app.js'))
     .pipe(gulp.dest('dist/js'))
-    .pipe(notify({ message: 'App task complete' }));
+    .pipe(notify({ message: 'App js task complete' }));
 })
 
-gulp.task('styles', function(){
-  return gulp.src('source/less/style.less')
+gulp.task('styles:main', function(){
+  return gulp.src('source/styles/main/style.less')
     .pipe(less()).on("error", notify.onError("Error: <%= error.message %>"))
     .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
     .pipe(gulp.dest('dist/css'))
@@ -29,6 +29,17 @@ gulp.task('styles', function(){
     .pipe(gulp.dest('dist/css'))
     .pipe(server.notify())
     .pipe(notify({ message: 'Styles task complete' }));
+})
+
+gulp.task('styles:bootstrap', function(){
+  return gulp.src('source/styles/bootstrap/bootstrap.less')
+    .pipe(less()).on("error", notify.onError("Error: <%= error.message %>"))
+    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+    .pipe(gulp.dest('dist/css'))
+    .pipe(minifycss())
+    .pipe(gulp.dest('dist/css'))
+    .pipe(server.notify())
+    .pipe(notify({ message: 'Bootstrap styles task complete' }));
 })
 
 gulp.task('views', function(){
@@ -39,8 +50,9 @@ gulp.task('views', function(){
       .on('error', notify.onError("Error: <%= error.message %>"))
       .pipe(gulp.dest('dist/'))
       .pipe(server.notify())
-      .pipe(notify({ message: 'Views task complete' }));
+      .pipe(notify({ message: 'Views jade task complete' }));
 })
+
 
 gulp.task('build',function(){
   gulp.run('styles','views')
@@ -54,9 +66,14 @@ gulp.task('watch', function(){
       gulp.run('app');
   });
 
-  gulp.watch(['source/less/**/*.less'],function(e){
-      gulp.run('styles');
+  gulp.watch(['source/styles/main/**/*.less'],function(e){
+      gulp.run('styles:main');
   });
+
+  gulp.watch(['source/styles/bootstrap/**/*.less'],function(e){
+      gulp.run('styles:bootstrap');
+  });
+
   gulp.watch(['source/views/**/*.jade'],function(e){
       gulp.run('views');
   });
---
+++ source/application/modules/topsellers/topsellers.controller.js
@@ -0,0 +1,23 @@
+$('#cardslider').slick({
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  arrows: true,
+  responsive: [
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    }
+  ]
+});
